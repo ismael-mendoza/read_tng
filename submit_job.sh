4e98b4520974d203bbcfc966bbf884d0278a9891
Ismael Mendoza <imendoza@umich.edu>
2024-04-19 17:35:37 -0400

    change job slightly

--- submit_job.sh
+++ submit_job.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-#SBATCH --job-name=tng100_1
+#SBATCH --job-name=tng50_3
 #SBATCH --output=%j.out
 #SBATCH --time=05:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem-per-cpu=180GB
+#SBATCH --mem-per-cpu=100GB
 #SBATCH --mail-type=END,FAIL
 #SBATCH --mail-user=imendoza@umich.edu
 #SBATCH --account=cavestru0
 #SBATCH --partition=standard
 module load python3.10-anaconda/2023.03
-python3 small_match.py
+python3 small_match_tng50_3.py
