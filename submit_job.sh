5b95cad833fcb18c8329839f8cd917d18043925a
Ismael Mendoza <imendoza@umich.edu>
2024-04-25 09:39:47 -0400

    final sbatch script that works

--- submit_job.sh
+++ submit_job.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH --job-name=tng100-test1
+#SBATCH --job-name=tng100-test2
 #SBATCH --output=%j.out
-#SBATCH --time=00:30:00
+#SBATCH --time=02:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
