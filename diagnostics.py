77be56f977944e82b5ecbb10f17cab77e05525c1
Ismael Mendoza <imendoza@umich.edu>
2025-07-13 18:04:04 -0400

    more diagnostics

--- diagnostics.py
+++ diagnostics.py
@@ -8,9 +8,9 @@ import matplotlib.pyplot as plt
 TNG_H = 0.6774  # from website
 FIG_DIR = Path("/home/imendoza/workspace/read_tng/plots/diagnostics")
 
-def _convert_tng_mass(m):
-   """Convert TNG mass to log10(Msun)."""
-   return np.where(m > 0, np.log10(m * 1e10 / TNG_H), 0)
+# def _convert_tng_mass(m):
+#    """Convert TNG mass to log10(Msun)."""
+#    return np.where(m > 0, np.log10(m * 1e10 / TNG_H), 0)
 
 with open('tng100_trees.npz', 'rb') as f:
     npz_data = np.load(f)
@@ -19,17 +19,20 @@ with open('tng100_trees.npz', 'rb') as f:
     bdmo = npz_data['bdmo']
     tbar = npz_data['tbar']
     tdmo = npz_data['tdmo']
+    matches = npz_data['matches']
 
 print("Finished loading data...")
 
+print("Number of matched hydro subhaloes:", len(matches[matches!=-1]))
+print("Percentage of matched hydro subhaloes", len(matches[matches!=-1]) / len(matches))
 
 # histogram
 fig, ax = plt.subplots(1, 1, figsize=(7,7))
-ax.hist(_convert_tng_mass(bbar['mpeak_pre']), bins=21)
+ax.hist(np.log10(bbar['mpeak_pre']), bins=21)
 fig.savefig(FIG_DIR / "mpeak_pre_bar_hist.png")
 
 fig, ax = plt.subplots(1, 1, figsize=(7,7))
-ax.hist(_convert_tng_mass(bdmo['mpeak_pre']), bins=21)
+ax.hist(np.log10(bdmo['mpeak_pre']), bins=21)
 fig.savefig(FIG_DIR / "mpeak_pre_dmo_hist.png")
 
 
@@ -42,13 +45,36 @@ mpeak_bar = bbar['mpeak'][mask]
 mpeak_pre_bar = bbar['mpeak_pre'][mask]
 
 fig, ax = plt.subplots(1, 1, figsize=(7,7))
-ax.plot(_convert_tng_mass(mpeak_pre_bar), mpeak_pre_bar / m_now_bar, '-o')
+ax.plot(np.log10(mpeak_pre_bar), mpeak_pre_bar / m_now_bar, 'o', alpha=0.1, markersize=2)
 ax.set_xlabel("m_peak_pre (log10 Msun)")
 ax.set_ylabel("m_peak_pre / m_now")
+ax.set_yscale("log")
 fig.savefig(FIG_DIR / "mpeak_pre_bar_ratio.png")
 
 fig, ax = plt.subplots(1, 1, figsize=(7,7))
-ax.plot(_convert_tng_mass(mpeak_bar), mpeak_bar / m_now_bar)
+ax.plot(np.log10(mpeak_bar), mpeak_bar / m_now_bar, 'o', alpha=0.1, markersize=2)
 ax.set_xlabel("m_peak (log10 Msun)")
 ax.set_ylabel("mpeak / m_now")
+ax.set_yscale("log")
 fig.savefig(FIG_DIR / "mpeak_bar_ratio.png")
+
+
+# dmo
+mask = (tdmo['mdm'][:, -1] > 0) & (bdmo['mpeak'] > 0) & (bdmo['mpeak_pre'] > 0) & (~bdmo['is_err'])
+m_now = tdmo['mdm'][:, -1][mask]
+mpeak = bdmo['mpeak'][mask]
+mpeak_pre = bdmo['mpeak_pre'][mask]
+
+fig, ax = plt.subplots(1, 1, figsize=(7,7))
+ax.plot(np.log10(mpeak_pre), mpeak_pre / m_now, 'o', alpha=0.1, markersize=2)
+ax.set_xlabel("m_peak_pre (log10 Msun)")
+ax.set_ylabel("m_peak_pre / m_now")
+ax.set_yscale("log")
+fig.savefig(FIG_DIR / "mpeak_pre_dmo_ratio.png")
+
+fig, ax = plt.subplots(1, 1, figsize=(7,7))
+ax.plot(np.log10(mpeak), mpeak / m_now, 'o', alpha=0.1, markersize=2)
+ax.set_xlabel("m_peak (log10 Msun)")
+ax.set_ylabel("mpeak / m_now")
+ax.set_yscale("log")
+fig.savefig(FIG_DIR / "mpeak_dmo_ratio.png")
